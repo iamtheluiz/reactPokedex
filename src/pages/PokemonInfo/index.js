166df93aa57c53a8c9574669ae4329bb16b1e61d
iamtheluiz <iamtheluiz.dev@gmail.com>
2019-07-05 04:08:00 -0300

    Added pokemon types

--- src/pages/PokemonInfo/index.js
+++ src/pages/PokemonInfo/index.js
@@ -11,7 +11,27 @@ import './styles.css';
 
 export default class PokemonInfo extends Component{
     state = {
-        pokemonInfo: {}
+        pokemonInfo: {
+            id: "",
+            name: "",
+            front_default: "",
+            stats: [],
+            types: [],
+        }
+    }
+
+    capitalize = (text) => {
+        if(text.indexOf('-') !== -1){
+            var words = text.split('-');
+            var capitalize = [];
+            words.forEach(word => {
+                capitalize.push(word.charAt(0).toUpperCase() + word.slice(1));
+            });
+            text = capitalize.join(" ");
+        }else{
+            text = text.charAt(0).toUpperCase() + text.slice(1);
+        }
+        return text;
     }
 
     componentDidMount(){
@@ -19,13 +39,23 @@ export default class PokemonInfo extends Component{
     }
 
     loadPokemonInfo = async () => {
-        const response = await api.get(`https://pokeapi.co/api/v2/pokemon/${this.props.match.params.id}`);
-        const { name, sprites: {front_default} } = response.data;
+        const response = await api.get(`https://pokeapi.co/api/v2/pokemon/${this.props.match.params.id}/`);
+
+        const { id, name, sprites: { front_default }, stats, types } = response.data;
+
+        var pokemonTypes = [];
+
+        types.forEach(type => {
+            pokemonTypes.push(this.capitalize(type.type.name));
+        });
 
         this.setState({
             pokemonInfo: {
+                id,
                 name,
-                front_default
+                front_default,
+                stats: stats,
+                types: pokemonTypes
             }
         });
     }
@@ -37,8 +67,14 @@ export default class PokemonInfo extends Component{
                 <div id="pokemonInfo">
                     <div className="basicInfo">
                         <img src={this.state.pokemonInfo.front_default} alt={this.state.pokemonInfo.name} />
-                        <p>{this.state.pokemonInfo.name}</p>
+                        <p>{this.state.pokemonInfo.name} - #{this.state.pokemonInfo.id}</p>
+                        {this.state.pokemonInfo.types.map((type) => (
+                            <span key={type} className={type.toLowerCase()}>{type}</span>
+                        ))}
                     </div>
+                    {this.state.pokemonInfo.stats.map((element) => (
+                        <p key={element.stat.name}><b>{this.capitalize(element.stat.name)}</b> => {element.base_stat}</p>
+                    ))}
                 </div>
             </>
         );
