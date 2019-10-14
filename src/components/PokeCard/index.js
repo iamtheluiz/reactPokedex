5c8eb7d83f1a511d41066e56f45701cfa7c6c910
iamtheluiz <iamtheluiz.dev@gmail.com>
2019-10-14 15:14:12 -0300

    Add working pokemon list

--- src/components/PokeCard/index.js
+++ src/components/PokeCard/index.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './styles.css';
 
 export default function PokeCard(props) {
+  const [pokeNum, setPokeNum] = useState(props.index.toString().split('').length);
+
+  useEffect(() => {
+    let num = '';
+
+    console.log();
+
+    for (let i = pokeNum; i < 3; i++) {
+      num += '0';
+    }
+
+    num += props.index;
+    setPokeNum(num);
+  }, []);
+
   return (
     <div className="pokeCard">
       <strong>{props.name}</strong>
       {props.type.map(type => <span key={type} className="pokeType">{type}</span>)}
-      <img src={props.src} alt="Pokemon"/>
+      <img src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${pokeNum}.png`} alt="Pokemon"/>
     </div>
   );
 }
--- src/components/PokeList/index.js
+++ src/components/PokeList/index.js
@@ -1,32 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 import PokeCard from '../../components/PokeCard';
 
 import './styles.css';
 
-export default function pokeList() {
+export default function PokeList() {
+  const [pokemons, setPokemons] = useState([]);
+  const [pokeCount, setPokeCount] = useState(0);
+  const [nextPage, setNextPage] = useState('https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0');
+
+  // Get info from pokeapi
+  useEffect(() => {
+    getPokeInfo();
+  }, []);
+
+  async function getPokeInfo() {
+    const { data } = await axios.get(nextPage);
+
+    const { count, next, results } = data;
+
+    setPokemons([...pokemons, ...results]);
+    setPokeCount(count);
+    setNextPage(next);
+  }
+
+  function handleLoadMore() {
+    getPokeInfo();
+  }
+
   return (
     <div id="pokeList">
-      <PokeCard
-        name="Bulbasaur"
-        type={["Grass", "Poison"]}
-        src="https://upload.wikimedia.org/wikipedia/pt/0/0c/001_Bulbasaur.png"
-      />
-      <PokeCard
-        name="Bulbasaur"
-        type={["Grass", "Poison"]}
-        src="https://upload.wikimedia.org/wikipedia/pt/0/0c/001_Bulbasaur.png"
-      />
-      <PokeCard
-        name="Bulbasaur"
-        type={["Grass", "Poison"]}
-        src="https://upload.wikimedia.org/wikipedia/pt/0/0c/001_Bulbasaur.png"
-      />
-      <PokeCard
-        name="Bulbasaur"
-        type={["Grass", "Poison"]}
-        src="https://upload.wikimedia.org/wikipedia/pt/0/0c/001_Bulbasaur.png"
-      />
+      {pokemons.map((pokemon, index) => (
+        <PokeCard
+          key={index}
+          index={index + 1}
+          name={pokemon.name}
+          type={["Grass", "Poison"]}
+        />
+      ))}
+      {nextPage !== null ? (
+        <button onClick={handleLoadMore}>More!</button>
+      ) : (
+        <p>End!</p>
+      )}
     </div>
   );
 }
